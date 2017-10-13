ed3e25f7922e8ec832587c3d7922ba8b29f908df
Jordan Daniels <decentpianist@gmail.com>
2017-10-13 15:24:17 -0700

    pictures render in front of user x: x+=.2, y:1.5 z: -.6

--- index.ios.js
+++ index.ios.js
@@ -14,88 +14,37 @@ export default class ReactNativeARKit extends Component {
           onPlaneDetected={console.log} // event listener for plane detection
           onPlaneUpdate={console.log} // event listener for plane update
         >
-          <ARKit.Box
-            position={{ x: 0, y: 0, z: 0 }}
-            shape={{ width: 0.1, height: 0.1, length: 0.1, chamfer: 0.01 }}
-          />
-      
-          <ARKit.Sphere
-            position={{ x: 0.2, y: 0, z: 0 }}
-            shape={{ radius: 0.05 }}
-          />
+          
          <ARKit.Model
-            position={{ x: -0.5, y: 5, z: 0, frame: 'local' }}
+            position={{ x: 0, y: 1.5, z: -.6, frame: 'local' }}
+            eulerAngle={{ x: 0, y: 0, z: 0 }}
             model={{
-              file: 'art.scnassets/head.psd', // make sure you have the model file in the ios project
+              file: 'art.scnassets/head.dae', // make sure you have the model file in the ios project
               scale: 0.01,
             }}
-          />
-          <ARKit.Cone
-            position={{ x: 0, y: 0.2, z: 0 }}
-            shape={{ topR: 0, bottomR: 0.05, height: 0.1 }}
-          />
-          <ARKit.Pyramid
-            position={{ x: 0.2, y: 0.15, z: 0 }}
-            shape={{ width: 0.1, height: 0.1, length: 0.1 }}
-          />
-          <ARKit.Pyramid
-            position={{ x: 1.2, y: 1.15, z: 0 }}
-            shape={{ width: 0.1, height: 0.1, length: 0.1 }}
-          />
-          <ARKit.Tube
-            position={{ x: 0.4, y: 0.2, z: 0 }}
-            shape={{ innerR: 0.03, outerR: 0.05, height: 0.1 }}
-          />
-          <ARKit.Torus
-            position={{ x: 0, y: 0.4, z: 0 }}
-            shape={{ ringR: 0.06, pipeR: 0.02 }}
-          />
-          <ARKit.Pyramid
-            position={{ x: 1.2, y: 1.15, z: 0 }}
-            shape={{ width: 0.1, height: 0.1, length: 0.1 }}
-          />
-          <ARKit.Capsule
-            position={{ x: 0.2, y: 0.4, z: 0 }}
-            shape={{ capR: 0.02, height: 0.06 }}
-          />
-          <ARKit.Plane
-            position={{ x: 0.4, y: 0.4, z: 0 }}
-            shape={{ width: 0.1, height: 0.1 }}
-          />
-          <ARKit.Model
-            position={{ x: -0.2, y: 0, z: 0, frame: 'local' }}
-            model={{
-              file: 'art.scnassets/head.copy.scn', // make sure you have the model file in the ios project
-              scale: 0.01,
-            }}
-          />
-          <ARKit.Model
-            position={{ x: 0, y: 0, z: 0, frame: 'local' }}
+          /><ARKit.Model
+            position={{ x: 0.2, y: 1.5, z: -.6, frame: 'local' }}
+            eulerAngle={{ x: 0, y: 0, z: 0 }}
             model={{
               file: 'art.scnassets/head.dae', // make sure you have the model file in the ios project
               scale: 0.01,
             }}
           />
           <ARKit.Model
-            position={{ x: -1, y: 0, z: 0, frame: 'local' }}
+            position={{ x: 0.4, y: 1.5, z: -.6, frame: 'local' }}
+            eulerAngle={{ x: 0, y: 0, z: 0 }}
             model={{
               file: 'art.scnassets/head.dae', // make sure you have the model file in the ios project
-              scale: 0.001,
+              scale: 0.01,
             }}
           />
-            <ARKit.Model
-            position={{ x: -1.4, y: 0, z: 0, frame: 'local' }}
-            model={{
-              file: 'art.scnassets/head.dae', // make sure you have the model file in the ios project
-              scale: 0.05,
-            }}
-          />
+          
           
           
 
          <ARKit.Text
-            text="change"
-            position={{ x: 0.2, y: 0.6, z: 0 }}
+            text="Hiiii Zoe ~"
+            position={{ x: 0.2, y: 0.6, z: 0,frame: 'local'  }}
             font={{ size: 0.15, depth: 0.05 }}
           />
         </ARKit>
@@ -105,8 +54,13 @@ export default class ReactNativeARKit extends Component {
 }
 
 AppRegistry.registerComponent('ReactNativeARKit', () => ReactNativeARKit);
+//art.scnassets/head.psd   
+//art.scnassets/head.dae
 
-// <ARKit.Box
+// art.scnassets/head.dae works!!!!
+
+//
+//           <ARKit.Box
 //             position={{ x: 0, y: 0, z: 0 }}
 //             shape={{ width: 0.1, height: 0.1, length: 0.1, chamfer: 0.01 }}
 //           />
@@ -142,6 +96,7 @@ AppRegistry.registerComponent('ReactNativeARKit', () => ReactNativeARKit);
 //             position={{ x: 0.4, y: 0.4, z: 0 }}
 //             shape={{ width: 0.1, height: 0.1 }}
 //           />
+
  // <ARKit.Model
  //            position={{ x: -0.2, y: 0, z: 0, frame: 'local' }}
  //            model={{
